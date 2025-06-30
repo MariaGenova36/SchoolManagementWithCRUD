2577e1ebb72b2d687b990fdea38b24a8e152b393
MariaGenova36 <mariq.genova@gmail.com>
2025-06-30 08:31:33 +0300

    Added Services classes

--- SchoolManagementWithCRUD/Program.cs
+++ SchoolManagementWithCRUD/Program.cs
@@ -1,71 +1,43 @@
-﻿using SchoolManagementWithCRUD;
-using System;
-using System.Linq;
-using SchoolManagementWithCRUD.Models;
+﻿using SchoolManagementConsole.Data;
+using SchoolManagementConsole.Services;
+using SchoolManagementWithCRUD.Services;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         using var context = new SchoolDbContext();
+        var studentService = new StudentService(context);
+        var subjectService = new SubjectService(context);
+        var enrollmentService = new EnrollmentService(context);
 
-        context.Database.EnsureCreated();
+        // Добавяне на ученици
+        studentService.AddStudent("Ivan Petrov", 11);
+        studentService.AddStudent("Elena Dimitrova", 12);
 
-        if (!context.Students.Any())
-        {
-            var student1 = new Student { Name = "Ivan Petrov", Grade = 11 };
-            var student2 = new Student { Name = "Elena Dimitrova", Grade = 12 };
+        // Добавяне на предмети
+        subjectService.AddSubject("Mathematics", "Mr. Stoyanov");
+        subjectService.AddSubject("Chemistry", "Mrs. Tsvetkova");
 
-            var subject1 = new Subject { Title = "Mathematics", Teacher = "Mr. Stoyanov" };
-            var subject2 = new Subject { Title = "Chemistry", Teacher = "Mrs. Tsvetkova" };
+        // Добавяне на записвания
+        enrollmentService.AddEnrollment(1, 1); // Иван - Математика
+        enrollmentService.AddEnrollment(2, 2); // Елена - Химия
 
-            context.Students.AddRange(student1, student2);
-            context.Subjects.AddRange(subject1, subject2);
+        // Показване
+        studentService.ListStudents();
+        subjectService.ListSubjects();
+        enrollmentService.ListEnrollments();
 
-            context.SaveChanges();
+        // Изтриване на първия ученик (Id = 1)
+        studentService.DeleteStudent(1);
 
-            // Записване на учениците в предметите
-            context.Enrollments.Add(new Enrollment { StudentId = student1.Id, SubjectId = subject1.Id });
-            context.Enrollments.Add(new Enrollment { StudentId = student1.Id, SubjectId = subject2.Id });
-            context.Enrollments.Add(new Enrollment { StudentId = student2.Id, SubjectId = subject1.Id });
+        // Преименуване на предмет с Id = 1
+        subjectService.EditSubjectTitle(1, "English");
 
-            context.SaveChanges();
-        }
-
-        // Извеждане на учениците
-        Console.WriteLine("Students:");
-        foreach (var student in context.Students)
-        {
-            Console.WriteLine($"ID: {student.Id} | Name: {student.Name} | Grade: {student.Grade}");
-        }
-
-        // Извеждане на предметите
-        Console.WriteLine("\nSubjects:");
-        foreach (var subject in context.Subjects)
-        {
-            Console.WriteLine($"ID: {subject.Id} | Title: {subject.Title} | Teacher: {subject.Teacher}");
-        }
-
-        // Изтриване на първия ученик и съответните му записи
-        var firstStudent = context.Students.FirstOrDefault();
-        if (firstStudent != null)
-        {
-            // Премахваме записите за него в Enrollments
-            var enrollments = context.Enrollments.Where(e => e.StudentId == firstStudent.Id);
-            context.Enrollments.RemoveRange(enrollments);
-
-            context.Students.Remove(firstStudent);
-            context.SaveChanges();
-            Console.WriteLine($"\nDeleted student ID {firstStudent.Id}");
-        }
-
-        // Редактиране на името на първия предмет (Mathematics -> English)
-        var firstSubject = context.Subjects.FirstOrDefault();
-        if (firstSubject != null)
-        {
-            firstSubject.Title = "English";
-            context.SaveChanges();
-            Console.WriteLine($"Updated subject ID {firstSubject.Id} title to {firstSubject.Title}");
-        }
+        // Показване след промени
+        Console.WriteLine("\nAfter changes:");
+        studentService.ListStudents();
+        subjectService.ListSubjects();
+        enrollmentService.ListEnrollments();
     }
 }
