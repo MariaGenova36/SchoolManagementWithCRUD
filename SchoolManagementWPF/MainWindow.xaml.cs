c19e6f587224ce822f71ce5d40bb2a008f29eaaa
MariaGenova36 <mariq.genova@gmail.com>
2025-09-29 14:18:01 +0300

    More Screenshots

--- SchoolManagementWPF/MainWindow.xaml.cs
+++ SchoolManagementWPF/MainWindow.xaml.cs
@@ -1,14 +1,43 @@
-﻿using SchoolManagementWPF;
+﻿using SchoolManagementWithCRUD.Services;
+using SchoolManagementWPF;
+using System.IO;
 using System.Windows;
 
 namespace SchoolManagementWPF
 {
     public partial class MainWindow : Window
     {
+        private readonly StudentService _studentService;
+        private readonly SubjectService _subjectService;
+        private readonly EnrollmentService _enrollmentService;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            var context = new SchoolDbContext();
+
+            _studentService = new StudentService(context);
+            _subjectService = new SubjectService(context);
+            _enrollmentService = new EnrollmentService(context);   
         }
+
+        private async void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var studentsText = await _studentService.GetStudentsText();
+            var subjectsText = await _subjectService.GetSubjectsText();
+            var enrollmentsText = await _enrollmentService.GetEnrollmentsText();
+
+            await File.WriteAllTextAsync("Students.txt", studentsText);
+            await File.WriteAllTextAsync("Subjects.txt", subjectsText);
+            await File.WriteAllTextAsync("Enrollments.txt", enrollmentsText);
+
+            MessageBox.Show("All data exported to Students.txt, Subjects.txt, Enrollments.txt",
+                            "Export Completed",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+        }
+
 
         private void OpenStudents_Click(object sender, RoutedEventArgs e)
         {
